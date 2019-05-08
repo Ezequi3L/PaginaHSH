87463a48e46a4be9d5cca69bbc1074b899ad8576
tomas mankowski <tomy-bsso.16@hotmail.com>
2019-05-07 21:58:50 -0300

    nico update migraciones

---
+++ database/migrations/2019_05_04_221536_create_ofertas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOfertasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ofertas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('monto');
+            $table->string('mail'); //mientras no tengamos usuarios, las ofertas se realizarán con una dirección de mail
+           // $table->foreign('usr_id')->references('id')->on('users');
+            $table->foreign('subasta_id')->references('id')->on('subastas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ofertas');
+    }
+}
