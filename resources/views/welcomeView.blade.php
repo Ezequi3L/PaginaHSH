acbeb332d95ee8666a258dda8a6a5958fb255195
Nicolás Fernandez <naf.1996@outlook.com.ar>
2019-05-07 21:03:08 -0300

    Migraciones agregadas

---
+++ resources/views/welcomeView.blade.php
@@ -0,0 +1,89 @@
+@extends('layout')
+
+@section('mainContent')
+
+ <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Home Switch Home</h1>
+      <p class="lead text-muted">Bienvenido. Aquí abajo le mostramos algunas de nuestras mejores residencias</p>
+      <p>
+        <a href="#" class="btn btn-primary my-2">Botón 1</a>
+        <a href="#" class="btn btn-secondary my-2">Botón 2</a>
+      </p>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Foto de residencia</text></svg>
+            <div class="card-body">
+              <p class="card-text">Acá iría una breve descripción de la residencia, o algo por el estilo.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
+
+@section('headerContent')
+
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+          <h4 class="text-white">About</h4>
+          <p class="text-muted">Hola giles!!!</p>
+        </div>
+        <div class="col-sm-4 offset-md-1 py-4">
+          <h4 class="text-white">Contáctenos</h4>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">221 888-8888</a></li>
+            <li><a href="#" class="text-white">Follow on Twitter</a></li>
+            <li><a href="#" class="text-white">Like on Facebook</a></li>
+            <li><a href="#" class="text-white">Email me</a></li>
+          </ul>
+        </div>
+      </div>
+
+@endsection
+
+@section('buscador')
+
+	<div class="container">
+    <br/>
+	<div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-8">
+            <form class="card card-sm" method="POST" action="/is2/server.php/">
+                <div class="card-body row no-gutters align-items-center">
+                    <div class="col-auto">
+                        <i class="fas fa-search h4 text-body"></i>
+                    </div>
+                       <!--end of col-->   
+                     <div class="col">
+                        <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Buscar residencias en subasta">
+                    </div>
+                    <!--end of col-->
+                    <div class="col-auto">
+                        <button class="btn btn-lg btn-success" type="submit" value="buscar">&#128270;</button>
+                    </div>
+                    <!--end of col-->
+                </div>
+            </form>
+        </div>
+        <!--end of col-->
+    </div>
+     <br/>
+	</div>
+
+@endsection
