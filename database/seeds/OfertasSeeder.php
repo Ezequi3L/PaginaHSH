4fbb15e2bb4b43baf9f09818b3555754dada5a6f
tomas mankowski <tomy-bsso.16@hotmail.com>
2019-05-23 09:03:58 -0300

    seeder de ofertasy residencias, falta subastas

--- database/seeds/OfertasSeeder.php
+++ database/seeds/OfertasSeeder.php
@@ -12,6 +12,48 @@ class OfertasSeeder extends Seeder
      */
     public function run()
     {
-      factory(App\Oferta::class)->times(17)->create();
+      //factory(App\Oferta::class)->times(17)->create();
+
+      Oferta::create(['monto' => 1000, 'email' => 'nico1@hotmail.com', subasta_id => 1
+
+      ]);
+
+      Oferta::create(['monto' => 1200, 'email' => 'nico2@hotmail.com', subasta_id => 1
+
+      ]);
+
+      Oferta::create(['monto' => 1400, 'email' => 'nico1@hotmail.com', subasta_id => 1
+
+      ]);
+
+      Oferta::create(['monto' => 1500, 'email' => 'nico2@hotmail.com', subasta_id => 1
+
+      ]);
+
+      Oferta::create(['monto' => 1350, 'email' => 'eze1@hotmail.com', subasta_id => 2
+
+      ]);
+
+      Oferta::create(['monto' => 1450, 'email' => 'eze2@hotmail.com', subasta_id => 2
+
+      ]);
+
+      Oferta::create(['monto' => 1900, 'email' => 'eze1@hotmail.com', subasta_id => 2
+
+      ]);
+
+      Oferta::create(['monto' => 850, 'email' => 'tomas1@hotmail.com', subasta_id => 3
+
+      ]);
+
+      Oferta::create(['monto' => 1000, 'email' => 'tomas2@hotmail.com', subasta_id => 3
+
+      ]);
+
+      Oferta::create(['monto' => 2000, 'email' => 'tomas1@hotmail.com', subasta_id => 3
+
+      ]);
+
+
     }
 }
--- database/seeds/ResidenciasSeeder.php
+++ database/seeds/ResidenciasSeeder.php
@@ -12,6 +12,30 @@ class ResidenciasSeeder extends Seeder
      */
     public function run()
     {
-      factory(App\Residencia::class)->times(17)->create();
+//      factory(App\Residencia::class)->times(17)->create();
+
+        Residencia::create([
+          'descripcion' => 'Residencia lujosa con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 14;
+        ]);
+
+        Residencia::create([
+          'descripcion' => 'Casa lujosa con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 1;
+        ]);
+
+        Residencia::create([
+          'descripcion' => 'Chalet lujosa con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 47;
+        ]);
+
+        Residencia::create([
+          'descripcion' => 'Departamento lujoso con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 73;
+        ]);
+
+        Residencia::create([
+          'descripcion' => 'Residencia lujosa con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 2;
+        ]);
+
+        Residencia::create([
+          'descripcion' => 'Cabaña lujosa con mucha ilumunacion y amplio espacio verde', 'localidad_id' => 3;
+        ]);
     }
 }
