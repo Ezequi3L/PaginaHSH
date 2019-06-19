0eb3efc24a75606fbc9da307659e6351e5e342cb
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2019-06-19 17:23:22 -0300

    solicitar y realizar upgrade

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,12 @@ Route::group( ['middleware' => 'auth' ], function() {  // rutas para las cuales 
 		Route::post('/usuario/verificar/{id}','UserController@check')
 			->name('check');
 
+		Route::get('/usuarios/upglist','UserController@listadoupg')
+			->name('listUpgUsr');
+
+		Route::post('/usuario/upgradear/{id}','UserController@upgrade')
+			->name('upgrade');
+
 		Route::get('/enviar/{destinatario}','MailController@sendMail')
 			->name('sendMail');
 
@@ -139,6 +145,9 @@ Route::group( ['middleware' => 'auth' ], function() {  // rutas para las cuales 
 
 		Route::delete('/reservas/{reserva}','ReservaController@cancelarReserva')
 			->name('deleteReserva');
+
+		Route::post('/solicitarUpg/{user}','UserController@solUpgrade')
+			->name('solUpgrade');
 
 	});
 
