214a5cae5e6952b9f0d7da88da9716b1ad19b587
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2019-06-18 21:23:44 -0300

    tabla usuarios por verificar

---
+++ database/factories/UsuariosPorVerificarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\UsuariosPorVerificar;
+use Faker\Generator as Faker;
+
+$factory->define(UsuariosPorVerificar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
