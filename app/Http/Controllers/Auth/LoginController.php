83fa813037d4cf38f7c289dcf5b71d92e07334ac
Ezequiel Dall'Aglio <ezequieldall@live.com.ar>
2019-06-22 19:50:34 -0300

    Un usuario dado de baja no puede loguearse

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -39,6 +40,11 @@ class LoginController extends Controller
 
     public function showLoginForm() {
         return view('auth/login',['title'=>'Login']);
+    }
+
+    protected function credentials(Request $Request){
+      $credentials = $Request->only($this->username(),'password');
+      return array_add($credentials, 'eliminado',0);
     }
 
 }
