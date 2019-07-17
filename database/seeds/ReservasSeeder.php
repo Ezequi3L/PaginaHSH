18984845b5637d8935ed7703061a8fe545eec14c
Nicolás Fernandez <naf.1996@outlook.com.ar>
2019-07-17 12:01:58 -0300

    Reserva directa sacada de usuario estándar

--- database/seeds/ReservasSeeder.php
+++ database/seeds/ReservasSeeder.php
@@ -19,7 +19,7 @@ class ReservasSeeder extends Seeder
           Reserva::create(['usr_id' => '3', 'residencia_id' => 19, 'fecha' => Carbon::now()->addMonths(3), 'hotsale' => '0', 'monto' => '10000']);
           //reservas que no deberían devolverle una semana (por menos de 2 meses):
           Reserva::create(['usr_id' => '2', 'residencia_id' => 23, 'fecha' => Carbon::now()->addMonths(1), 'hotsale' => '0', 'monto' => '5000']);
-          Reserva::create(['usr_id' => '3', 'residencia_id' => 4, 'fecha' => Carbon::now()->addMonths(1), 'hotsale' => '0']);
+          
           //reservas que no deberían devolverle una semana (por ser hotsale):
           Reserva::create(['usr_id' => '2', 'residencia_id' => 23, 'fecha' => Carbon::now()->addMonths(4), 'hotsale' => '1', 'monto' => '5000']);
           Reserva::create(['usr_id' => '3', 'residencia_id' => 19, 'fecha' => Carbon::now()->addMonths(4), 'hotsale' => '1', 'monto' => '15000']);
